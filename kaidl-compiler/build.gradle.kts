6c0f7216fa337d5a4e3e02cfccb6bf12db82ec48
Goooler <wangzongler@gmail.com>
2026-04-17 18:08:08 +0800

    Add KCT

--- kaidl-compiler/build.gradle.kts
+++ kaidl-compiler/build.gradle.kts
@@ -3,4 +3,8 @@ plugins { alias(libs.plugins.kotlin.jvm) }
 dependencies {
   compileOnly(libs.ksp.api)
   implementation(libs.kotlinpoet)
+
+  testImplementation(libs.assertk)
+  testImplementation(libs.kct.core)
+  testImplementation(libs.kct.ksp)
 }
